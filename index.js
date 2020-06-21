5e4ac72135e96f30eb997b7f33cea0672a4d65b6
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-21 21:11:32 +0530

    setup routes and controllers

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 8000;
 
 const app = express();
 
+app.use(express.urlencoded()); // to allow data to be understood by express
+
+// specifying assets folder
+app.use(express.static('./assets'));
+
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes/index')); // we can do './routes' also as it will automatically fetch index
+
 app.listen(port, function(err){
     if(err)
         console.log("Error in runnung the server", err);
